8b60520201d9abc56ee8515d16a8d51ef71f3449
tassad <Tassadaritze@gmail.com>
2023-06-12 16:48:25 +0300

    feat: /aon command

---
+++ src/commands/aon.rs
@@ -0,0 +1,52 @@
+use std::env;
+
+use anyhow::Result;
+use serde::Deserialize;
+
+use crate::Context;
+
+/// Search Archives of Nethys
+#[poise::command(
+    slash_command,
+    description_localized("ru", "Поиск по Archives of Nethys")
+)]
+pub async fn aon(
+    ctx: Context<'_>,
+    #[description = "What to search"]
+    #[description_localized("ru", "Что искать")]
+    #[name_localized("ru", "запрос")]
+    query: String,
+) -> Result<()> {
+    let reqwest = ctx.framework().user_data.reqwest.clone();
+
+    ctx.defer().await?;
+
+    let res = reqwest
+        .get(env::var("SEARCH_URL")?)
+        .query(&[
+            ("format", "json"),
+            ("q", &("site:aonprd.com ".to_string() + &query)),
+        ])
+        .send()
+        .await?
+        .json::<Response>()
+        .await?;
+
+    let response = match res.results.get(0) {
+        Some(result) => &result.url,
+        None => "No results.",
+    };
+    ctx.say(response).await?;
+
+    Ok(())
+}
+
+#[derive(Deserialize, Debug)]
+struct SearchResult {
+    url: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Response {
+    results: Vec<SearchResult>,
+}
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -3,16 +3,18 @@ use poise::Command;
 
 use crate::DataWrapper;
 use _8ball::_8ball;
+use aon::aon;
 use gelbooru::gelbooru;
 use ping::ping;
 use register::register;
 
 #[path = "8ball.rs"]
 mod _8ball;
+mod aon;
 mod gelbooru;
 mod ping;
 mod register;
 
 pub fn commands() -> Vec<Command<DataWrapper, Error>> {
-    vec![ping(), _8ball(), register(), gelbooru()]
+    vec![ping(), _8ball(), register(), gelbooru(), aon()]
 }
