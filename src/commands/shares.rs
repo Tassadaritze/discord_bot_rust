2fcac9acac9a89598ccfb58831e1dea00041c7ac
tassad <Tassadaritze@gmail.com>
2023-06-13 19:37:06 +0300

    feat: first forays into SQL h*ck

--- src/commands/shares.rs
+++ src/commands/shares.rs
@@ -1,4 +1,6 @@
 use anyhow::Result;
+use log::debug;
+use sqlx::{query, Row};
 
 use crate::Context;
 
@@ -10,6 +12,16 @@ pub async fn shares(_ctx: Context<'_>) -> Result<()> {
 /// View and collect shares
 #[poise::command(slash_command)]
 pub async fn get(ctx: Context<'_>) -> Result<()> {
+    let sqlite = ctx.data().sqlite.clone();
+
+    let row = query("SELECT * FROM share WHERE user_id = 1234")
+        .fetch_optional(&sqlite)
+        .await?;
+
+    if let Some(row) = row {
+        debug!("{:?}", row.try_get::<&str, &str>("user_id")?);
+    }
+
     ctx.say("Share view goes here").await?;
 
     Ok(())
